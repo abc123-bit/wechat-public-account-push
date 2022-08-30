f527f587fe8c062615beb0cf031af8ef3e9ddcc3
abc123-bit <76656440+abc123-bit@users.noreply.github.com>
2022-08-30 17:56:24 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,11 +1,14 @@
 export const config = {
+    /**
+     * 公众号配置
+     */
 
     // 公众号APP_ID
     APP_ID: "wx085d7f62d80a4890",
     // 公众号APP_SECRET
     APP_SECRET: "aa9184227312a0173ab7243feb7b7249",
     // 模板消息id
-    TEMPLATE_ID: "gWmEAH0VxlW3Fcnt33itEByPkrTgkKR3N20t-3i6lqU",
+    TEMPLATE_ID: "qGHVBFaRIdi-74N2yDiK0ifDLdcPgtnPrjq9odbAbQI",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     USERS: ["oO95X6xNiVFJiS0JNArFcXZhswEA"],
      
@@ -20,36 +23,53 @@ export const config = {
     // 所在城市
     CITY: "梧州",
 
-    /** 生日相关 */
+    /** 重要节日相关 */
 
-    // 生日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
-    BIRTHDAYS: [
-      {"name": "莹莹", "year": "2001", "date": "06-26"},
-      
+    // 重要节日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
+    // tpye必须填！ 只能 “生日” 和 “节日” 二选一!
+    // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
+    // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
+    FESTIVALS: [
+      {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
+      {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
+      {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
+      {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
+    ],
+    // 限制重要节日的展示条目, 需要填写数字; 
+    // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
+    // 如果为0, 则默认展示全部
+    FESTIVALS_LIMIT: 3,
 
     /** 日期相关 */
 
     // 在一起的日子，格式同上
-    LOVE_DATE:"2016-06-09",
-        
+    LOVE_DATE: "2015-05-01",
+    // 结婚纪念日
+    MARRY_DATE: "2020-01-04",
 
 
-    /** 好文节选 */
+    /** 每日一言 */
 
     // 好文节选的内容类型
     // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-    LITERARY_PREFERENCE: "哈喽宝贝，又是爱你的一天哦~"
+    LITERARY_PREFERENCE: ""
 
 
     }
 
 // {{date.DATA}}  
 // 城市：{{city.DATA}}  
-// 天气：{{weather.DATA}} 
+// 天气：{{weather.DATA}}  
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
+// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
 // {{note_ch.DATA}}
+
+// 共推送 {{success_post_num.DATA}}  人
+// 成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
+// 成功用户: {{success_post_ids.DATA}}
+// 失败用户: {{fail_post_ids.DATA}}
